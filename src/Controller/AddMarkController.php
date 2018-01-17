0718f6c3a5e1f703acbb0aa5cf3cc16b9bce56b5
VANKOCKO <van.kocko@gmail.com>
2018-01-17 15:11:52 +0100

    addMark commit

---
+++ src/Controller/AddMarkController.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vankocko
+ * Date: 16/01/2018
+ * Time: 11:42
+ */
+
+namespace App\Controller;
+
+use App\Entity\Mark;
+use App\Form\AddMarkAddType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class AddMarkController extends  Controller
+{
+
+    /**
+     * @Route("Mark/AddMark/add", name="AddMark_add")
+     */
+    public function add(Request $request)
+    {
+
+        $mark = new Mark();
+
+
+
+        $form = $this->createForm(AddMarkAddType::class, $mark);
+
+
+        // Validation du formulaire
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $markSave = $form->getData();
+
+            $file =$markSave->getImage();
+            // Générer le nom de fichier
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+            // Déplacer le fichier temporaire vers le dossier uploads/
+            $file->move($this->getParameter('uploads_directory'), $fileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($markSave);
+            $em->flush();
+
+
+            return $this->redirectToRoute('mark_create_confirmation');
+        }
+
+        return $this->render('Back-Office/Mark/AddMark.html.twig', [
+            'formAdd' => $form->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("AddMark/add/ok", name="mark_create_confirmation")
+     */
+    public function addOk() {
+        return $this->render('Back-Office/Mark/add-Markconfirmation.html.twig');
+    }
+
+
+
+
+
+}
---
+++ src/Form/AddMarkAddType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Mark;
+use Doctrine\ORM\EntityRepository;
+use App\Entity\Media;
+use App\Entity\Question;
+use App\Entity\User;
+use Doctrine\DBAL\Types\DecimalType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
+use App\Entity\Route;
+
+
+class AddMarkAddType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+
+        $builder->add('name',TextType ::class , [
+				'label' => 'Ajouter un repère ',
+				'constraints' => [
+					new NotBlank(),
+				]
+			]
+        );
+        $builder->add('coordinateX', NumberType::class, [
+				'label' => 'Coordonnées X',
+				'constraints' => [
+					new NotBlank(),
+				]
+			]
+        );
+        $builder->add('coordinateY', NumberType::class, [
+				'label' => 'Coordonnées X ',
+				'constraints' => [
+					new NotBlank(),
+				]
+			]
+        );
+        $builder->add('image', FileType::class, [
+            'constraints' => [
+                new NotBlank(),
+                new File(['mimeTypes' => ['image/jpeg', 'image/png', 'image/gif']]),
+            ]
+        ]);
+        $builder->add('description', TextareaType::class, [
+				'label' => 'Description ',
+				'constraints' => [
+					new NotBlank(),
+				]
+			]
+        );
+        
+       $builder->add('save', SubmitType::class, ['label' => 'Ajouter']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+
+            'data_class' => Mark::class,
+        ]);
+    }
+    
+    
+}
