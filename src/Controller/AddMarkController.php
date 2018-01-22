c427e0b220b16dc414f7c9814ccaa424c77c89b8
VANKOCKO <van.kocko@gmail.com>
2018-01-22 15:39:44 +0100

    MapV2

--- src/Controller/AddMarkController.php
+++ src/Controller/AddMarkController.php
@@ -14,24 +14,29 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
+use App\Entity\Museum;
 class AddMarkController extends  Controller
 {
-
     /**
      * @Route("Mark/AddMark/add", name="AddMark_add")
      */
     public function add(Request $request)
     {
         $form = $this->createForm(AddMarkAddType::class);
+        //recuperer la map
+        $em=$this->getDoctrine()->getManager();
+        $maps=$em->getRepository(Museum::class)->findBy([],['id'=>'desc'],1);
 
 
+        foreach( $maps  as $key => $value )
+        {
+
+        }
         // Validation du formulaire
-
         $form->handleRequest($request);
 
-
         if ($form->isSubmitted() && $form->isValid()) {
+
 
             $markSave = $form->getData();
             $markQuestions = $markSave->getQuestions();
@@ -41,6 +46,9 @@ class AddMarkController extends  Controller
             // Générer le nom de fichier
             $fileName = md5(uniqid()) . '.' . $file->guessExtension();
             // Déplacer le fichier temporaire vers le dossier uploads/
+
+            $markSave->setImage($fileName);
+
             $file->move($this->getParameter('uploads_directory'), $fileName);
 
             foreach($markQuestions as $currentQuestion)
@@ -49,11 +57,10 @@ class AddMarkController extends  Controller
                 $currentQuestion->setAnswers($jsonAnswer);
             }
 
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($markSave);
             $em->flush();
-
-
 
 
             $lastId = $this->getDoctrine()->getRepository(Mark::class)->findBy([],['id'=>'desc'],1);
@@ -78,6 +85,7 @@ class AddMarkController extends  Controller
 
         return $this->render('Back-Office/Mark/AddMark.html.twig', [
             'formAdd' => $form->createView(),
+            'map'=>$maps
         ]);
 
     }
@@ -85,8 +93,10 @@ class AddMarkController extends  Controller
     /**
      * @Route("AddMark/add/ok", name="mark_create_confirmation")
      */
-    public function addOk() {
-        return $this->render('Back-Office/Mark/add-Markconfirmation.html.twig');
+    public function addOk() 
+    {
+        return $this->render('Back-Office/Mark/add-Markconfirmation.html.twig'
+        );
     }
 
 
